96e4b921b8571db4a8d6e3ba4a4a22f3d17e19ee
Nadav <nadavh19@gmail.com>
2025-06-26 19:11:42 +0300

    Start enforcing same album and band name for group

--- src/UserForm.js
+++ src/UserForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import './Styles/UserForm.css';
+ 
 
 function UserForm({ onSubmit }) {
   const [userData, setUserData] = React.useState({
